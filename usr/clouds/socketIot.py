0772d57188272cb5382861b913352b872bb4391b
Dustin Wei(韦伟) <dustin.wei@QUECTEL.COM>
2023-09-28 09:15:43 +0800

    add socket.py

--- usr/clouds/socketIot.py
+++ usr/clouds/socketIot.py
@@ -1,73 +1,9 @@
-import usocket
+from usr.common.socket import Socket
 from usr.common.threading import Thread, Queue
 from usr.common.logging import getLogger
 
 
 logger = getLogger(__name__)
-
-
-class Socket(object):
-
-    def __init__(self, host, port, timeout=5, protocol='TCP'):
-        self.__host = host
-        self.__port = port
-        self.__ip = None
-        self.__family = None
-        self.__domain = None
-        self.__timeout = timeout
-        self.__sock = None
-        if protocol == 'TCP':
-            self.__sock_type = usocket.SOCK_STREAM
-        else:
-            self.__sock_type = usocket.SOCK_DGRAM
-
-    def __str__(self):
-        if self.__sock is None:
-            return '<Socket Unbound>'
-        return '<{}({}:{})>'.format(
-            'TCP' if self.__sock_type == usocket.SOCK_STREAM else 'UDP',
-            self.__ip,
-            self.__port
-        )
-
-    def __init_args(self):
-        rv = usocket.getaddrinfo(self.__host, self.__port)
-        if not rv:
-            raise ValueError('DNS detect error for addr: {},{}.'.format(self.__host, self.__port))
-        self.__family = rv[0][0]
-        self.__domain = rv[0][3]
-        self.__ip, self.__port = rv[0][4]
-
-    def connect(self):
-        self.__init_args()
-        self.__sock = usocket.socket(self.__family, self.__sock_type)
-        self.__sock.settimeout(self.__timeout)
-        if self.__sock_type == usocket.SOCK_STREAM:
-            self.__sock.connect((self.__ip, self.__port))
-
-    def disconnect(self):
-        if self.__sock:
-            self.__sock.close()
-            logger.info('{} disconnect.'.format(self))
-            self.__sock = None
-
-    def is_status_ok(self):
-        if self.__sock:
-            if self.__sock_type == usocket.SOCK_STREAM:
-                return self.__sock.getsocketsta() == 4
-            else:
-                return True
-        return False
-
-    def write(self, data):
-        if self.__sock_type == usocket.SOCK_STREAM:
-            flag = (self.__sock.send(data) == len(data))
-        else:
-            flag = (self.__sock.sendto(data, (self.__ip, self.__port)) == len(data))
-        return flag
-
-    def read(self, size=1024):
-        return self.__sock.recv(size)
 
 
 class SocketIot(object):
